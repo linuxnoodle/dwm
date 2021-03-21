f211bdf8d30147a1eeb7440fbc63c27e0df17bfa
linuxnoodle <ipadnoodle@gmail.com>
2021-03-21 14:53:51 -0400

    Changed focus from lazy to onclick (manually applied patch)

--- dwm.c
+++ dwm.c
@@ -164,7 +164,7 @@ static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
-static void enternotify(XEvent *e);
+//static void enternotify(XEvent *e);
 static void expose(XEvent *e);
 static void focus(Client *c);
 static void focusin(XEvent *e);
@@ -183,7 +183,7 @@ static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
-static void motionnotify(XEvent *e);
+//static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *);
@@ -251,13 +251,13 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[ConfigureRequest] = configurerequest,
 	[ConfigureNotify] = configurenotify,
 	[DestroyNotify] = destroynotify,
-	[EnterNotify] = enternotify,
+	//[EnterNotify] = enternotify,
 	[Expose] = expose,
 	[FocusIn] = focusin,
 	[KeyPress] = keypress,
 	[MappingNotify] = mappingnotify,
 	[MapRequest] = maprequest,
-	[MotionNotify] = motionnotify,
+	//[MotionNotify] = motionnotify,
 	[PropertyNotify] = propertynotify,
 	[UnmapNotify] = unmapnotify
 };
@@ -435,7 +435,8 @@ buttonpress(XEvent *e)
 
 	click = ClkRootWin;
 	/* focus monitor if necessary */
-	if ((m = wintomon(ev->window)) && m != selmon) {
+	//if ((m = wintomon(ev->window)) && m != selmon) {
+    if ((m = wintomon(ev->window)) && m != selmon && (focusonwheel || (ev->button != Button4 && ev->button != Button5))){
 		unfocus(selmon->sel, 1);
 		selmon = m;
 		focus(NULL);
@@ -455,9 +456,11 @@ buttonpress(XEvent *e)
 		else
 			click = ClkWinTitle;
 	} else if ((c = wintoclient(ev->window))) {
-		focus(c);
-		restack(selmon);
-		XAllowEvents(dpy, ReplayPointer, CurrentTime);
+		//focus(c);
+		//restack(selmon);
+		if (focusonwheel || (ev->button != Button4 && ev->button != Button5))
+            focus(c);
+        XAllowEvents(dpy, ReplayPointer, CurrentTime);
 		click = ClkClientWin;
 	}
 	for (i = 0; i < LENGTH(buttons); i++)
@@ -762,24 +765,24 @@ drawbars(void)
 		drawbar(m);
 }
 
-void
-enternotify(XEvent *e)
-{
-	Client *c;
-	Monitor *m;
-	XCrossingEvent *ev = &e->xcrossing;
-
-	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
-		return;
-	c = wintoclient(ev->window);
-	m = c ? c->mon : wintomon(ev->window);
-	if (m != selmon) {
-		unfocus(selmon->sel, 1);
-		selmon = m;
-	} else if (!c || c == selmon->sel)
-		return;
-	focus(c);
-}
+//void
+//enternotify(XEvent *e)
+//{
+//	Client *c;
+//	Monitor *m;
+//	XCrossingEvent *ev = &e->xcrossing;
+//
+//	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+//		return;
+//	c = wintoclient(ev->window);
+//	m = c ? c->mon : wintomon(ev->window);
+//	if (m != selmon) {
+//		unfocus(selmon->sel, 1);
+//		selmon = m;
+//	} else if (!c || c == selmon->sel)
+//		return;
+//	focus(c);
+//}
 
 void
 expose(XEvent *e)
@@ -1128,22 +1131,22 @@ monocle(Monitor *m)
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
 }
 
-void
-motionnotify(XEvent *e)
-{
-	static Monitor *mon = NULL;
-	Monitor *m;
-	XMotionEvent *ev = &e->xmotion;
-
-	if (ev->window != root)
-		return;
-	if ((m = recttomon(ev->x_root, ev->y_root, 1, 1)) != mon && mon) {
-		unfocus(selmon->sel, 1);
-		selmon = m;
-		focus(NULL);
-	}
-	mon = m;
-}
+//void
+//motionnotify(XEvent *e)
+//{
+//	static Monitor *mon = NULL;
+//	Monitor *m;
+//	XMotionEvent *ev = &e->xmotion;
+//
+//	if (ev->window != root)
+//		return;
+//	if ((m = recttomon(ev->x_root, ev->y_root, 1, 1)) != mon && mon) {
+//		unfocus(selmon->sel, 1);
+//		selmon = m;
+//		focus(NULL);
+//	}
+//	mon = m;
+//}
 
 void
 movemouse(const Arg *arg)
